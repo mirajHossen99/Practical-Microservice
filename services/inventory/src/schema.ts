98313eb6ff2b4f8a76020abbdd9d9e722d61b541
mirajHossen99 <mirajhossen011211@gmail.com>
2026-04-26 01:30:55 +0600

    Inventory DTO schema added

--- services/inventory/src/schema.ts
+++ services/inventory/src/schema.ts
@@ -3,5 +3,8 @@ import { z } from "zod";
 export const InventoryDTOSchema = z.object({
     productId: z.string(),
     sku: z.string(),
-    quantity: z.number().int().positive()
-})
+    quantity: z.number().int().positive().optional().default(0)
+    
+});
+
+export type InventoryCreateDTO = z.infer<typeof InventoryDTOSchema>;
