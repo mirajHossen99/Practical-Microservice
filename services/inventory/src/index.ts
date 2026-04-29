e716dc9d7cbd63b043e4c8a5c2d50a2bbb80919c
mirajHossen99 <mirajhossen011211@gmail.com>
2026-04-29 11:57:04 +0600

    Added Controller into index

--- services/inventory/src/index.ts
+++ services/inventory/src/index.ts
@@ -1,43 +1,46 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import morgan from 'morgan';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import morgan from "morgan";
 
-import { createInventory, getInventoryById, getInventoryDetails, updateInventory } from "./controllers/index";
+import {
+  createInventory,
+  getInventoryById,
+  getInventoryDetails,
+  updateInventory,
+} from "./controllers/index";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/health', (_req, res) => {
-    res.status(200).json({ status: 'UP'});
+app.get("/health", (_req, res) => {
+  res.status(200).json({ status: "UP" });
 });
 
 // routes
-app.get('/inventories/:id/details', getInventoryDetails);
-app.get('/inventories/:id', getInventoryById);
-app.put('/inventories/:id', updateInventory);
-app.post('/inventories', createInventory);
-
-
+app.get("/inventories/:id/details", getInventoryDetails);
+app.get("/inventories/:id", getInventoryById);
+app.put("/inventories/:id", updateInventory);
+app.post("/inventories", createInventory);
 
 // 404 handler
 app.use((_req, res) => {
-    res.status(404).json({ message: 'Not found'});
+  res.status(404).json({ message: "Not found" });
 });
 
 // Error handler
 app.use((err, _req, res, _next) => {
-    console.error(err.stack);
-    res.status(500).json({ message: 'Internal server error' });
-})
+  console.error(err.stack);
+  res.status(500).json({ message: "Internal server error" });
+});
 
-const port = process.env.PORT || 4002
-const serviceName = process.env.SERVICE_NAME || 'Inventory-Service';
+const port = process.env.PORT || 4002;
+const serviceName = process.env.SERVICE_NAME || "Inventory-Service";
 
 app.listen(port, () => {
-    console.log(`${serviceName} is running on port ${port}`);
-})
+  console.log(`${serviceName} is running on port ${port}`);
+});
