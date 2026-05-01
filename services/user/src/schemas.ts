87102206285f9c0c0e826cc8e3f947705482fc85
mirajHossen99 <mirajhossen011211@gmail.com>
2026-05-01 16:06:47 +0600

    Schemas added

--- services/user/src/schemas.ts
+++ services/user/src/schemas.ts
@@ -1,5 +1,21 @@
-import { z } from "zod";
+import { email, z } from "zod";
 
+export const UserCreateDTOSchema = z.object({
+  authUserId: z.string(),
+  name: z.string(),
+  email: z.string().email(),
+  address: z.string().optional(),
+  phone: z.string().optional(),
+});
 
+export const UserUpdateDTOSchema = UserCreateDTOSchema.omit({
+  authUserId: true,
+}).partial();
 
+// export const UserUpdateDTOSchema = z.object({
 
+//     name: z.string().optional(),
+//     email: z.string().email().optional(),
+//     address: z.string().optional(),
+//     phone: z.string().optional(),
+// });
